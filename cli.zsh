38a65b49108ecfd452273cdb1f269c892b5be1f9
Anthony Briglia <ambriglia@gmail.com>
2021-04-10 13:23:20 -0400

    add starting files

---
+++ cli.zsh
@@ -0,0 +1,12 @@
+pwd
+ls
+ls -al
+cd repos/
+cd ../
+cd repos/
+mkdir test
+cd test
+touch test.txt
+rm test.txt
+mkdir someDir
+rm -r someDir
---
+++ git.zsh
@@ -0,0 +1,6 @@
+git clone git@github.com:ambriglia/webdev-tutorial.git
+cd webdev-tutorial
+# make changes
+git add .
+git commit -m "a message describing changes"
+git push origin main
