8bb8d0e981d5b1317f06152d4ac2b46d52eef8eb
Anthony Briglia <ambriglia@gmail.com>
2021-04-12 16:38:38 -0400

    js fun

---
+++ two/bonanza.js
@@ -0,0 +1,44 @@
+var name = "anthony";
+const age = 35;
+const isTeaching = true;
+const isSleeping = false;
+
+if (isTeaching) {
+  console.log('Anthony likes to teach!');
+} else if (isSleeping) {
+  console.log('Anthony is sleeping');
+} else {
+  console.log('No on knows what Anthony is doing...');
+}
+
+if (age >= 30 && name === 'anthony') {
+  console.log('Anthony is an old man');
+}
+
+const hobbies = ['reading', 'video games', 'teaching'];
+console.log(hobbies);
+
+console.log(`first element: ${hobbies[0]}`);
+console.log(hobbies.length);
+hobbies.push('running');
+
+for (let i = 0; i < hobbies.length; i++) {
+  const hobby = hobbies[i];
+  console.log(`in a for loop: ${hobby}`);
+}
+
+const person = {
+  name: 'pete',
+  age: 40,
+  isTeaching: false,
+  isSleeping: true,
+};
+
+console.log(`the person's name is ${person.name} and their age is ${person.age}`);
+
+function add(x, y) {
+  return x + y;
+}
+
+const result = add(1, 2);
+console.log(result);
