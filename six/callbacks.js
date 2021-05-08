099cdb7411048bfd7a087e24d134811340a31795
Anthony Briglia <ambriglia@gmail.com>
2021-05-08 11:22:34 -0400

    week 6 first

---
+++ six/callbacks.js
@@ -0,0 +1,23 @@
+function addHello(name, cb) {
+  const helloMessage = `Hello ${name}!`;
+  cb(helloMessage);
+}
+
+addHello('Anthony', function (message) {
+  console.log(message);
+});
+
+const arrayCallback = (element, index) => {
+  console.log(`element: ${element}`);
+  console.log(`index: ${index}`);
+}
+
+const numbers = [2, 6, 5, 3, 6, 7, 8, 1];
+console.log(numbers);
+
+console.log('----for loop----');
+for (let i = 0; i < numbers.length; i++) {
+  arrayCallback(numbers[i], i);
+}
+console.log('---foreach----');
+numbers.forEach(arrayCallback);
