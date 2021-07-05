0118892c044d28def0f6401e8a12efe619c11ae8
Anthony Briglia <ambriglia@gmail.com>
2021-07-05 14:41:01 -0400

    rest/spread

---
+++ eleven/spreadRest.js
@@ -0,0 +1,32 @@
+const set = [1, 2];
+const anotherSet = [3, 4];
+const union = [...set, ...anotherSet];
+console.log(union);
+
+const foo = {
+  bar: 'baz',
+  number: 42,
+};
+const boo = {
+  bar: 'bot',
+  alive: false,
+};
+
+const fooBoo = {
+  ...foo,
+  ...boo,
+};
+console.log(fooBoo);
+
+function add(...params) {
+  let sum = 0;
+  for (let i = 0; i < params.length; i++) {
+    const element = params[i];
+    sum += element;
+  }
+  return sum;
+}
+
+console.log(add(1, 2, 3));
+console.log(add(1));
+console.log(add(1, 2, 3, 4, 5, 6, 7, 8));
