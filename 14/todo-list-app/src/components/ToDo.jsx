1aa275e7b6d340b7f179aec534e0f59acda6df14
Anthony Briglia <ambriglia@gmail.com>
2021-07-27 20:30:51 -0400

    create new todo text state variable.

--- 14/todo-list-app/src/components/ToDo.jsx
+++ 14/todo-list-app/src/components/ToDo.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react';
 
 const ToDo = () => {
   const [toDos, setToDos] = useState([]);
+  const [newToDoText, setNewToDoText] = useState('');
+
+  const onChangeHandler = (event) => {
+    if (event.target.name === 'newToDoText') {
+      setNewToDoText(event.target.value);
+    }
+  }
 
   return (
     <>
       <h1>Welcome to the todo list application!</h1>
       <form>
-        <input type="text" />
+        <input
+          type="text"
+          name="newToDoText"
+          value={newToDoText}
+          onChange={onChangeHandler} />
       </form>
       {toDos.length <= 0
         ? <p>Add todos to see your list</p>
