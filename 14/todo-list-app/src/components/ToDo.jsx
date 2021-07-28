d5182ec9424df270fd276a32434ca4391d28af0f
Anthony Briglia <ambriglia@gmail.com>
2021-07-27 20:38:53 -0400

    implement onSubmit handler

--- 14/todo-list-app/src/components/ToDo.jsx
+++ 14/todo-list-app/src/components/ToDo.jsx
@@ -10,21 +10,37 @@ const ToDo = () => {
     }
   }
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!newToDoText) {
+      return;
+    }
+
+    setToDos([...toDos, newToDoText]);
+    setNewToDoText('');
+  }
+
   return (
     <>
       <h1>Welcome to the todo list application!</h1>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <input
           type="text"
           name="newToDoText"
           value={newToDoText}
           onChange={onChangeHandler} />
+        <button type="submit">Create ToDo!</button>
       </form>
       {toDos.length <= 0
         ? <p>Add todos to see your list</p>
         : (
           <ul>
-            <li>a todo</li>
+            {toDos.map((todo, index) => {
+              return (
+                <li key={index}>{todo}</li>
+              )
+            })}
           </ul>
         )
       }
