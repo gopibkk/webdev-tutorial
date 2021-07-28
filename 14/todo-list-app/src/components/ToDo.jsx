6ffbf0d8623ed0f93860cfb5627aac06e8b145c6
Anthony Briglia <ambriglia@gmail.com>
2021-07-27 20:25:23 -0400

    introduce state into todo component. conditionally render content if no todos

--- 14/todo-list-app/src/components/ToDo.jsx
+++ 14/todo-list-app/src/components/ToDo.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
 const ToDo = () => {
-  return <h1>Hello from ToDo app</h1>;
+  const [toDos, setToDos] = useState([]);
+
+  return (
+    <>
+      <h1>Welcome to the todo list application!</h1>
+      <form>
+        <input type="text" />
+      </form>
+      {toDos.length <= 0
+        ? <p>Add todos to see your list</p>
+        : (
+          <ul>
+            <li>a todo</li>
+          </ul>
+        )
+      }
+    </>
+  )
 }
 
 export default ToDo;
