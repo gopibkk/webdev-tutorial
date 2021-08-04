ef2129ac05b7ff23eda1fe0d26630533308f7574
Anthony Briglia <ambriglia@gmail.com>
2021-08-03 21:01:35 -0400

    fav gifs

--- 15/giphy-app/src/App.js
+++ 15/giphy-app/src/App.js
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Gif from './components/Gif';
+import SearchGif from './components/SearchGif';
 
 function App() {
   const [gifs, setGifs] = useState([]);
+  const [favoriteGifs, setFavoriteGifs] = useState([]);
 
   useEffect(() => {
     const getTrendingGifs = async () => {
@@ -14,14 +16,35 @@ function App() {
     getTrendingGifs();
   }, []);
 
+  const searchGifs = async (searchText) => {
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=r9lryrT1sb53mewKob2973KLcJiNh1bO&q=${searchText}`);
+    setGifs(response.data.data);
+  }
+
+  const saveFavorite = (gifId) => {
+    const favGif = gifs.find(gif => gif.id === gifId);
+    setFavoriteGifs([...favoriteGifs, favGif]);
+  }
+
   return (
     <>
       <h1>Giphy App!</h1>
-      {gifs.map((gif, index) => (
+      <SearchGif searchGifs={searchGifs} />
+      <p>Favorite gifs</p>
+      {favoriteGifs.map((gif) => (
         <Gif
-          key={index}
+          key={gif.id}
           imageUrl={gif.images.fixed_height.url}
           title={gif.images.title} />
+      ))}
+      <p>Trending or searched gifs</p>
+      {gifs.map((gif) => (
+        <Gif
+          key={gif.id}
+          imageUrl={gif.images.fixed_height.url}
+          title={gif.images.title}
+          id={gif.id}
+          saveFavorite={saveFavorite} />
       ))}
       {/* component to capture the search form
       component to render all the trending gifs
